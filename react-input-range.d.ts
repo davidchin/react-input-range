0ba6d6cb8a1267a326354e946d77fff494183985
Nicolas Schmitt <nicolas.schmitt@amundi.com>
2017-01-04 11:30:25 +0100

    minor update to d.ts to make it typescript v2 compliant

--- react-input-range.d.ts
+++ react-input-range.d.ts
@@ -4,9 +4,12 @@
 // The legacy 'require'-syntax  is preferred, since the module isn't
 // ES6-Standard.
 
-declare module 'react-input-range' {
-  export = ReactInputRange.InputRange;
-}
+/// <reference types="react" />
+
+import {ClassicComponentClass} from 'react';
+
+declare let InputRange: ClassicComponentClass<ReactInputRange.IInputRangeProps>;
+export = InputRange;
 
 declare namespace ReactInputRange {
   interface IRange {
@@ -31,19 +34,15 @@ declare namespace ReactInputRange {
     ariaControls?: string;
     defaultValue?: number;
     disabled?: boolean;
-    formatLabel?:
-      (labelValue: string, parameters: { labelPrefix: string, labelSuffix: string}) => string;
-
+    formatLabel?: (labelValue: string, parameters: {labelPrefix: string, labelSuffix: string}) => string;
     labelPrefix?: string;
     labelSuffix?: string;
     maxValue?: number;
     minValue?: number;
     name?: string;
-    onChange: (element: this, value: (number | IRange)) => any;
-    onChangeComplete?: () => any;
+    onChange: (element: this, value: (number | IRange)) => void;
+    onChangeComplete?: () => void;
     step?: number;
     value: number | IRange;
   }
-
-  export class InputRange extends __React.Component<IInputRangeProps, any> {}
 }
