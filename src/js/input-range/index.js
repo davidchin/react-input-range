b254d1aa1b2a2b69129662fa2f980d2e83b8ad43
davidchin <davidchin.yf@gmail.com>
2017-01-26 09:33:53 +1100

    style: Re-export module

---
+++ src/js/input-range/index.js
@@ -0,0 +1,1 @@
+export { default as InputRange } from './input-range';
--- src/js/input-range/input-range.jsx
+++ src/js/input-range/input-range.jsx
@@ -9,28 +9,6 @@ import { autobind, captialize, distanceTo, isDefined, isObject, length } from '.
 import { DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, UP_ARROW } from './key-codes';
 
 /**
- * An object describing the position of a point
- * @typedef {Object} Point
- * @property {number} x - x value
- * @property {number} y - y value
- */
-
-/**
- * An object describing a range of values
- * @typedef {Object} Range
- * @property {number} min - Min value
- * @property {number} max - Max value
- */
-
-/**
- * @typedef {Object} ClientRect
- * @property {number} height - Height value
- * @property {number} left - Left value
- * @property {number} top - Top value
- * @property {number} width - Width value
- */
-
-/**
  * InputRange React component
  */
 export default class InputRange extends React.Component {
