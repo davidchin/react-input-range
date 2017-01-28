6e6abeb0dbec89278b4c9483edf0dd2743377c5d
davidchin <davidchin.yf@gmail.com>
2017-01-28 21:01:40 +1100

    refactor: Remove unneccessary helpers

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,16 +1,8 @@
-/**
- * @type {Function} predicateFn
- * @param {*} value
- * @return {boolean}
- */
-
 export { default as autobind } from './autobind';
 export { default as captialize } from './captialize';
 export { default as clamp } from './clamp';
 export { default as distanceTo } from './distance-to';
 export { default as isDefined } from './is-defined';
-export { default as isEmpty } from './is-empty';
 export { default as isNumber } from './is-number';
 export { default as isObject } from './is-object';
 export { default as length } from './length';
-export { default as objectOf } from './object-of';
