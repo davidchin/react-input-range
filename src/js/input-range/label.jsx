7615f9443c23a617e6d6e0369eb87530b712ddc3
prasanna1211 <prasanna.krystal@gmail.com>
2017-04-20 10:29:33 +0530

    added build

--- src/js/input-range/label.jsx
+++ src/js/input-range/label.jsx
@@ -9,10 +9,10 @@ import React from 'react';
  */
 export default function Label(props) {
   const labelValue = props.formatLabel ? props.formatLabel(props.children, props.type) : props.children;
-
+  const labelClassName = props.markerType === 'track' ? `${props.classNames.labelContainer} marker-${props.markerValue}` : props.classNames.labelContainer;
   return (
     <span className={props.classNames[`${props.type}Label`]}>
-      <span className={props.classNames.labelContainer}>
+      <span className={labelClassName}>
         {labelValue}
       </span>
     </span>
@@ -31,4 +31,7 @@ Label.propTypes = {
   classNames: React.PropTypes.objectOf(React.PropTypes.string).isRequired,
   formatLabel: React.PropTypes.func,
   type: React.PropTypes.string.isRequired,
+  markerType: React.PropTypes.string,
+  markerValue: React.PropTypes.string,
+
 };
